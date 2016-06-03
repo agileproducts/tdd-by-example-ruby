976cffa4475985db0875cc34916467f4bd4e2bd0
Stephen Cornelius <stephencornelius@brians-air.home>
2016-06-03 21:59:29 +0100

    chapter 11 minus redundant minitests

--- money_minitest.rb
+++ money_minitest.rb
@@ -1,10 +1,11 @@
 require 'minitest/autorun'
 require_relative 'money'
 
-class TestDollar < Minitest::Test
+class TestMoney < Minitest::Test
 
   def setup
     @fivedollars = Money.dollar(5)
+    @fivefrancs = Money.franc(5)
   end
 
   def test_multiplication
@@ -12,34 +13,9 @@ class TestDollar < Minitest::Test
     assert_equal Money.dollar(10), tendollars
     fifteendollars = @fivedollars.times(3)
     assert_equal Money.dollar(15), fifteendollars
-  end
-
-  def test_equality
-    assert Money.dollar(5).==(Money.dollar(5))
-    refute Money.dollar(5).==(Money.dollar(6))
-  end
-
-end
-
-class TestFranc < Minitest::Test
-
-  def setup
-    @fivefrancs = Money.franc(5)
-  end
-
-  def test_multiplication
     tenfrancs = @fivefrancs.times(2)
     assert_equal Money.franc(10), tenfrancs
   end
-
-  def test_equality
-    assert Money.franc(5).==(Money.franc(5))
-    refute Money.franc(5).==(Money.franc(6))
-  end
-
-end
-
-class TestMoney < Minitest::Test
 
   def test_equality
     assert Money.dollar(5).==(Money.dollar(5))
@@ -53,5 +29,7 @@ class TestMoney < Minitest::Test
     assert "USD" == Money.dollar(1).currency
     assert "CHF" == Money.franc(1).currency
   end
-  
+
+
+
 end
